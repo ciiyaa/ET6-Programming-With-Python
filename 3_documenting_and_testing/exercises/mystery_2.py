2d9438744c4582efd5a2791c6fb08ce912140b7d
ciiyaa <c.aavah@gmail.com>
2025-01-05 21:56:33 +0300

    Mystery2 docstring

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for counting characters in a string.
+
+Module contents:
+    - mystery_2: Accepts a string and returns its length.
+"""
 def mystery_2(a):
+    """Accepts a string and returns its length.
+
+    Parameters:
+        a: str, a string to find the length of
+
+    Returns -> int: length of characters
+    """
     if len(a) == 0:
         return None
 
     return len(a)
+print (mystery_2("zzzz!s1 zz"))

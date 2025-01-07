4408132496224d5a8ea4f7ccde247d3abebd7aef
ciiyaa <c.aavah@gmail.com>
2025-01-08 02:30:17 +0300

    Add edge cases TestTwoNumbersCompare

--- 3_documenting_and_testing/exercises/tests/test_two_numbers_compare.py
+++ 3_documenting_and_testing/exercises/tests/test_two_numbers_compare.py
@@ -5,6 +5,7 @@ Test module for two_numbers_compare function.
 
 Test categories:
     - Standard cases: Typical integers with different relationships.
+    - Edge cases: Negative numbers.
 """
 
 import unittest
@@ -31,3 +32,16 @@ class TestTwoNumbersCompare(unittest.TestCase):
     def test_a_equal_b(self):
         """Should return their sum when a == b"""
         self.assertEqual(two_numbers_compare(10, 10), 20)
+
+    # Edge Cases
+    def test_negative_positive(self):
+        """Should handle a negative, b positive"""
+        self.assertEqual(two_numbers_compare(-7, 5), -7)
+
+    def test_positive_negative(self):
+        """Should handle a positive, b negative"""
+        self.assertEqual(two_numbers_compare(5, -3), -3)
+
+    def test_negative_negative(self):
+        """Should handle negative numbers correctly"""
+        self.assertEqual(two_numbers_compare(-3, -5), -5)

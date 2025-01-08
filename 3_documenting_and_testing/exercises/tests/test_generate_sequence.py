9f93879b186c5a85e1ef1e9dd8a27d7af7e20401
ciiyaa <c.aavah@gmail.com>
2025-01-08 21:08:07 +0300

    MAdd edge test generate_sequence

--- 3_documenting_and_testing/exercises/tests/test_generate_sequence.py
+++ 3_documenting_and_testing/exercises/tests/test_generate_sequence.py
@@ -5,6 +5,7 @@ Test module for the generate_sequence function.
 
 Test categories:
     - Standard cases: Positive integers for generating sequences.
+    - Edge cases: Minimal input.
 """
 
 import unittest
@@ -16,10 +17,6 @@ class TestGenerateSequence(unittest.TestCase):
     """Test generate_sequence function"""
 
     # Standard Cases
-    def test_minimal_input(self):
-        """It should return an empty list for minimal input 0"""
-        self.assertEqual(generate_sequence(0), [])
-
     def test_generate_sequence_length_5(self):
         """Should generate a list of 5 integers"""
         self.assertEqual(generate_sequence(5), [0, 1, 2, 3, 4])
@@ -27,3 +24,8 @@ class TestGenerateSequence(unittest.TestCase):
     def test_generate_sequence_length_3(self):
         """Should generate a list of 3 integers"""
         self.assertEqual(generate_sequence(3), [0, 1, 2])
+
+    # Edge Cases
+    def test_minimal_input(self):
+        """It should return an empty list for minimal input 0"""
+        self.assertEqual(generate_sequence(0), [])

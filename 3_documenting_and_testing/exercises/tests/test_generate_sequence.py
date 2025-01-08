8fddd33381ca3ff7d9b24915cd5d6eb1451dd4c3
ciiyaa <c.aavah@gmail.com>
2025-01-08 21:10:05 +0300

    Add edge tests generate_sequence

--- 3_documenting_and_testing/exercises/tests/test_generate_sequence.py
+++ 3_documenting_and_testing/exercises/tests/test_generate_sequence.py
@@ -6,6 +6,7 @@ Test module for the generate_sequence function.
 Test categories:
     - Standard cases: Positive integers for generating sequences.
     - Edge cases: Minimal input.
+    - Defensive tests: Non-integer or negative inputs assertions.
 """
 
 import unittest
@@ -29,3 +30,19 @@ class TestGenerateSequence(unittest.TestCase):
     def test_minimal_input(self):
         """It should return an empty list for minimal input 0"""
         self.assertEqual(generate_sequence(0), [])
+
+    # Defensive Tests
+    def test_invalid_type_string(self):
+        """Should raise an AssertionError for string input"""
+        with self.assertRaises(AssertionError):
+            generate_sequence("5")
+
+    def test_invalid_type_float(self):
+        """Should raise an AssertionError for float input"""
+        with self.assertRaises(AssertionError):
+            generate_sequence(5.5)
+
+    def test_negative_input(self):
+        """Should raise an AssertionError for negative input"""
+        with self.assertRaises(AssertionError):
+            generate_sequence(-5)

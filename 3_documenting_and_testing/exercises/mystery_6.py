80807567265ff115c6225d9da7afe28e8f0138a7
ciiyaa <c.aavah@gmail.com>
2025-01-09 15:08:02 +0300

    Add docstring mys6

--- 3_documenting_and_testing/exercises/mystery_6.py
+++ 3_documenting_and_testing/exercises/mystery_6.py
@@ -1,4 +1,33 @@
-def mystery_6(a, b):
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for generating a sequence based on specific rules.
+
+Module contents:
+    - generate_sequence: Generates a sequence starting from `b`, with `a` elements.
+"""
+
+
+def generate_sequence(a: int, b: int) -> list:
+    """Generates a sequence of `a` elements, starting from `b`.
+
+    Parameters:
+        a (int): The number of elements to generate.
+        b (int): The starting value of the sequence.
+
+    Returns -> list: A list of `a` elements, starting from `b`.
+
+    Raises:
+        AssertionError: If `a` or `b` is not an integer or if `a` is negative.
+
+    Examples:
+    >>> generate_sequence(0, 0)
+    []
+    >>> generate_sequence(3, 5)
+    [5, 6, 7]
+    >>> generate_sequence(2, -1)
+    [-1, 0]
+    """
     if a == 0:
         return []
 

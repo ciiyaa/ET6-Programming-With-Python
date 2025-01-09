010bfc3eb0db30481ee91230bb1ee372f6fe5c63
ciiyaa <c.aavah@gmail.com>
2025-01-09 15:33:47 +0300

    Add docstring mys7

--- 3_documenting_and_testing/exercises/mystery_7.py
+++ 3_documenting_and_testing/exercises/mystery_7.py
@@ -1,4 +1,31 @@
-def mystery_7(a, b):
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for filtering a list of strings based on the presence of a specified substring.
+
+Module contents:
+    - filter_containing_substring: Filters a list of strings, returning only those that
+    contain a given substring.
+"""
+
+
+def filter_containing_substring(a: list[str], b: str) -> list[str]:
+    """Filters a list of strings, returning only those that contain a specified substring.
+
+    Parameters:
+        strings (list[str]): List of strings to filter.
+        substring (str): Substring to search for in each string.
+
+    Returns -> list[str]: A list of strings from the input list that contain the substring.
+
+    Examples:
+    >>> filter_containing_substring(["apple", "banana", "cherry"], "ana")
+    ['banana']
+    >>> filter_containing_substring(["dog", "cat", "rabbit"], "cat")
+    ['cat']
+    >>> filter_containing_substring(["one", "two", "three"], "")
+    ['one', 'two', 'three']
+    """
     c = []
     for d in a:
         if b in d:

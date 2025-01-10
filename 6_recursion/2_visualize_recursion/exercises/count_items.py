73998c2df7d5b62a84d32b56e4792ace178f4842
ciiyaa <c.aavah@gmail.com>
2025-01-10 16:35:14 +0300

    Add count_items.py docstring and comments

--- 6_recursion/2_visualize_recursion/exercises/count_items.py
+++ 6_recursion/2_visualize_recursion/exercises/count_items.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-""" Visualizing count_items
+"""Visualizing count_items
 
-To visualize implementation, 
+To visualize implementation,
 - use your VSCode debugger
 - copy-paste the code into PythonTutor
 
@@ -29,22 +29,28 @@ from trace_recursion import trace_recursion
 @trace_recursion
 def count_items(to_count: list) -> int:
     """
-    
+    Counts the number of items in a list using recursion.
+
+    base case:
+        If the list is empty, return 0.
+    recursive case:
+        Return 1 plus the count of the rest of the list.
     """
-    if len(to_count) == 0:
+    if len(to_count) == 0:  # base case
         return 0
 
+    # recursive case
     break_down = to_count[1:]
     recursion = count_items(break_down)
     build_up = recursion + 1
 
-    return  build_up
+    return build_up
 
 
 # --- call the traced function ---
 
-print(count_items([]), 'should be', 0)
-print(count_items([1, 2, 3]), 'should be', 3)
-print(count_items([1, 2, 1]), 'should be', 3)
-print(count_items(["", False, None, 0]), 'should be', 4)
-print(count_items(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']), 'should be', 8)
+print(count_items([]), "should be", 0)
+print(count_items([1, 2, 3]), "should be", 3)
+print(count_items([1, 2, 1]), "should be", 3)
+print(count_items(["", False, None, 0]), "should be", 4)
+print(count_items(["a", "b", "c", "d", "e", "f", "g", "h"]), "should be", 8)
